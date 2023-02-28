3cd2931e9adf1c58ec02c5ffdc9d1daa544b5441
Molodkina Nelly <molodkinanelli@gmail.com>
2023-02-28 17:40:12 +0300

    Добавила решение задачи 23

--- Program.cs
+++ Program.cs
@@ -4,14 +4,21 @@
 //12821 -> да
 //32 -> да
 
-// Console.WriteLine("Введите пятизначное число: ");
-//string number = Console.ReadLine();
-//string str = Convert.ToString(number);
-//int lgt = str!.Length;
-//if (str[0]==str[4] && str[1]==str[3])
-//Console.WriteLine("Число палиндром  ");
-//else
-//Console.WriteLine("Число не палиндром");
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+string str = Convert.ToString(number);
+int lgt = str!.Length;
+//while (true)
+//{
+ //   if (str!.Length = 5)
+ //       break;
+  //  else 
+  //      Console.WriteLine("Число должно быть пятизначным!");
+//}
+if (str[0]==str[4] && str[1]==str[3])
+Console.WriteLine("Число палиндром  ");
+else
+Console.WriteLine("Число не палиндром");
 
 
 //Задача 21.Напишите программу, которая принимает на вход координаты двух точек и 
@@ -34,9 +41,16 @@ result = (double)Math.Sqrt(Math.Pow((pointB[0] - pointA[0]), 2) + Math.Pow((poin
 Console.WriteLine("Расстояние между точками составляет:  " + result);
 
 
-
-
-
 //Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите любое число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+for (i = 1; i <= Num; i++)
+{
+    res = i*i*i;
+    Console.WriteLine(res);
+}
+
